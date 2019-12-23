642564627744b2f836f6ae80efe37ad0cfcf60d7
oliver alejandro almaguer tostado <almaguero95@gmail.com>
2019-12-22 16:47:15 -0800

    Updates

---
+++ precache-manifest.0bccbf79c57fecbbe9f4e77a41f0a5a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29ee9cd7bea32c382bfd1b95d1cc9847",
+    "url": "/index.html"
+  },
+  {
+    "revision": "83ac04c0fab56f705e13",
+    "url": "/static/css/main.f9740d80.chunk.css"
+  },
+  {
+    "revision": "755b755f665dc3091178",
+    "url": "/static/js/2.1feb1ce0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.1feb1ce0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "83ac04c0fab56f705e13",
+    "url": "/static/js/main.1ab1f054.chunk.js"
+  },
+  {
+    "revision": "fc42dcb99f23617c8b27",
+    "url": "/static/js/runtime-main.7b0edef7.js"
+  }
+]);
